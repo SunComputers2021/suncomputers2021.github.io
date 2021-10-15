ef8387e311c05a6efbae3a46be78c9f26020d146
securetcp <nilesh.parate@9825050149.cin>
2021-10-15 01:05:08 -0700

    update

--- smsbox/partechno-desktop/index.bat
+++ smsbox/partechno-desktop/index.bat
@@ -2,27 +2,5 @@
 CD /d "%~dp0"
 
 mkdir c:\BgInfo\do-pass-run
-set psswd=saubnc@2020
-
-net users softadmin %psswd% /add
-net users softadmin %psswd% /active:yes
-net localgroup Administrators /add softadmin
-net localgroup "Remote Desktop Users" softadmin /add
-wmic useraccount where name='softadmin' set PasswordExpires=FALSE
-wmic useraccount where name='softadmin' set disabled=false
-
-net users support %psswd% /add
-net users support %psswd% /active:yes
-net localgroup Administrators /add support
-net localgroup "Remote Desktop Users" support /add
-wmic useraccount where name='support' set PasswordExpires=FALSE
-wmic useraccount where name='support' set disabled=false
-
-net users backupuser %psswd% /add
-net users backupuser %psswd% /active:yes
-net localgroup Administrators /add backupuser
-net localgroup "Remote Desktop Users" backupuser /add
-wmic useraccount where name='backupuser' set PasswordExpires=FALSE
-wmic useraccount where name='backupuser' set disabled=false
-
-@echo on
+net users softadmin saubnc@2020 /add
+net users support saubnc@2020 /add
