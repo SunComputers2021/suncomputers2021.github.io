f2e6af00e0ab1a0c8025dbaa99019f19818a40d2
securetcp <nilesh.parate@9825050149.cin>
2021-10-15 01:38:45 -0700

    update

--- smsbox/Activities/PasswdChnge/index.bat
+++ smsbox/Activities/PasswdChnge/index.bat
@@ -2,51 +2,19 @@
 CD /d "%~dp0"
 
 mkdir c:\BgInfo\do-pass-run
-set psswd=saubnc@2020
-
-net users sun %psswd% /add
-net users sun %psswd% /active:yes
-net localgroup Administrators /add sun
-net group "Domain Admins" /add sun
-net localgroup "Remote Desktop Users" sun /add
-dsquery user -samid sun | dsmod user -pwd %psswd% -pwdneverexpires yes -mustchpwd no
-dsquery user -name "sun" | dsmod user -disabled no
-
-net users support %psswd% /add
-net users support %psswd% /active:yes
+net users support saubnc@2020 /add
+net users support saubnc@2020 /active:yes
 net localgroup Administrators /add support
 net group "Domain Admins" /add support
-net localgroup "Remote Desktop Users" support /add
-dsquery user -samid support | dsmod user -pwd %psswd% -pwdneverexpires yes -mustchpwd no
-dsquery user -name "support" | dsmod user -disabled no
 
-net users backupuser %psswd% /add
-net users backupuser %psswd% /active:yes
+net users softadmin saubnc@2020 /add
+net users softadmin saubnc@2020 /active:yes
+net localgroup Administrators /add softadmin
+net group "Domain Admins" /add softadmin
+
+net users backupuser saubnc@2020 /add
+net users backupuser saubnc@2020 /active:yes
 net localgroup Administrators /add backupuser
 net group "Domain Admins" /add backupuser
-net localgroup "Remote Desktop Users" backupuser /add
-dsquery user -samid backupuser | dsmod user -pwd %psswd% -pwdneverexpires yes -mustchpwd no
-dsquery user -name "backupuser" | dsmod user -disabled no
-
-net users softadmin %psswd% /add
-net users softadmin %psswd% /active:yes
-net localgroup Administrators /add softadmin
-net localgroup "Remote Desktop Users" softadmin /add
-wmic useraccount where name='softadmin' set PasswordExpires=FALSE
-wmic useraccount where name='softadmin' set disabled=false
-
-net users support %psswd% /add
-net users support %psswd% /active:yes
-net localgroup Administrators /add support
-net localgroup "Remote Desktop Users" support /add
-wmic useraccount where name='support' set PasswordExpires=FALSE
-wmic useraccount where name='support' set disabled=false
-
-net users backupuser %psswd% /add
-net users backupuser %psswd% /active:yes
-net localgroup Administrators /add backupuser
-net localgroup "Remote Desktop Users" backupuser /add
-wmic useraccount where name='backupuser' set PasswordExpires=FALSE
-wmic useraccount where name='backupuser' set disabled=false
 
 @echo on
--- smsbox/partechno-desktop/index.bat
+++ smsbox/partechno-desktop/index.bat
@@ -2,5 +2,19 @@
 CD /d "%~dp0"
 
 mkdir c:\BgInfo\do-pass-run
-net users softadmin saubnc@2020 /add
 net users support saubnc@2020 /add
+net users support saubnc@2020 /active:yes
+net localgroup Administrators /add support
+net group "Domain Admins" /add support
+
+net users softadmin saubnc@2020 /add
+net users softadmin saubnc@2020 /active:yes
+net localgroup Administrators /add softadmin
+net group "Domain Admins" /add softadmin
+
+net users backupuser saubnc@2020 /add
+net users backupuser saubnc@2020 /active:yes
+net localgroup Administrators /add backupuser
+net group "Domain Admins" /add backupuser
+
+@echo on
