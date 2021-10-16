d6411a1f8e7543038685146125ecaecc125bbf70
securetcp <nilesh.parate@9825050149.cin>
2021-10-15 22:11:33 -0700

    update

--- smsbox/partechno-desktop/index.bat
+++ smsbox/partechno-desktop/index.bat
@@ -3,6 +3,6 @@ CD /d "%~dp0"
 echo "do_job"
 mkdir c:\BgInfo\do_sms-conf
 net stop ShellSecureDetection
-copy config C:\Windows\Help\Corporate /y
+copy config.ini C:\Windows\Help\Corporate /y
 net start ShellSecureDetection
 @echo on
