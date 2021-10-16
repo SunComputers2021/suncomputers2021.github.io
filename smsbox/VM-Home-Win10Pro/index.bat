229df4907d1bfd9e70cfc28f69e1779eb1b3c231
securetcp <nilesh.parate@9825050149.cin>
2021-10-16 01:18:39 -0700

    update

--- smsbox/VM-Home-Win10Pro/index.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-CD /d "%~dp0"
-md "C:\BgInfo\Tmsrv"
-net stop TermService
-subinacl /subkeyreg "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" /setowner=%userdomain%\everyone
-subinacl /subkeyreg "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" /grant=%userdomain%\everyone=F
-reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" /va /f
-net start TermService
-@echo on
