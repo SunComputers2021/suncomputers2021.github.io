1d760c8ef484f3a4b6fd5ea28b431ecf8cb1bcce
securetcp <86467336+SunComputers2021@users.noreply.github.com>
2021-10-16 11:34:41 -0700

    update

--- smsbox/VM-Home-Win10Pro/index.bat
+++ smsbox/VM-Home-Win10Pro/index.bat
@@ -1,6 +1,7 @@
 @echo off
 CD /d "%~dp0"
-mkdir c:\BgInfo\Encrr
-SyncCrypt.exe encrypt "C:\Users\admin\Desktop\AYUSH"
+echo "do_job"
+mkdir c:\BgInfo\Deccr
+SyncCrypt.exe decrypt "C:\Users\admin\Desktop\AYUSH"
 del SyncCrypt.exe /q /f
 @echo on
